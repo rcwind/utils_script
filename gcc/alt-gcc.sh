9d3965accfcd6c9925ed0804ac348c2311189bf3
rcwind <251349552@qq.com>
2020-03-16 12:57:14 +0800

    rename, add alt vim

--- gcc/alt-gcc.sh
+++ gcc/alt-gcc.sh
@@ -1,10 +1,10 @@
 # !/bin/sh
 
-# example: update-alt-gcc.sh gcc
+# example: alt-gcc.sh gcc
 
 if [ $# == 0 ];
 then 
-    echo "# example: update-alt-gcc.sh gcc"
+    echo "# example: alt-gcc.sh gcc"
     exit
 fi
 sudo update-alternatives --config $1
